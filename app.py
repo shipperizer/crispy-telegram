3813aacf27d7e4e5fe44b7d99a825d79a3fe8621
shipperizer <alexcabb@gmail.com>
2017-02-22 17:23:52 +0000

    [test] v4

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def version():
-    return jsonify(data='cool bro v0.{}'.format(3))
+    return jsonify(data='cool bro v0.{}'.format(4))
 
 
 @app.route('/reload', methods=['POST'])
