18a97395035df98126aacea711d76beaa8ddfac3
shipperizer <alexcabb@gmail.com>
2017-02-22 17:13:07 +0000

    [fix]

--- Makefile
+++ Makefile
@@ -4,10 +4,11 @@ install:
 	pip install -r requirements.txt
 
 run:
-	FLASK_DEBUG=1 FLASK_APP=app.py gunicorn -w 4 -b 127.0.0.1:5000 app:app
+	FLASK_DEBUG=1 FLASK_APP=app.py gunicorn -w 4 -b 0.0.0.0:5000 app:app
+	pkill gunicorn
 
 reload:
-	wget --no-check-certificate https://raw.githubusercontent.com/shipperizer/crispy-telegram/master/app.py
+	wget --no-check-certificate https://raw.githubusercontent.com/shipperizer/crispy-telegram/master/app.py -O app.py
 
 build:
 	docker build . --tag shipperizer/crispy-telegram
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, current_app, request
 
 app = Flask(__name__)
@@ -7,14 +9,9 @@ app = Flask(__name__)
 def version():
     return jsonify(data='cool bro v0.{}'.format(1))
 
+
 @app.route('/reload', methods=['POST'])
 def reload():
     current_app.logger.warning('Restarting')
-    shutdown_server()
+    os.system('pkill gunicorn')
     return jsonify(data='echo off')
-
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
